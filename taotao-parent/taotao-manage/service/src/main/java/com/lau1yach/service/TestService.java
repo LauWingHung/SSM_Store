be6e29a2dd6b403d2b00fe76e523e3285ff5fd81
LauWingHung <lyhplayer@me.com>
2018-10-03 09:28:48 +0800

    update

--- taotao-parent/taotao-manage/service/src/main/java/com/lau1yach/service/TestService.java
+++ taotao-parent/taotao-manage/service/src/main/java/com/lau1yach/service/TestService.java
@@ -1,12 +1,5 @@
 package com.lau1yach.service;
 
-
-
-
 public interface TestService {
-    /**
-     * 从数据库中获取当前的时间
-     * @return
-     */
-    public String queryNow();
+    String queryNow();
 }
--- taotao-parent/taotao-manage/service/src/main/java/com/lau1yach/service/impl/TestServiceImpl.java
+++ taotao-parent/taotao-manage/service/src/main/java/com/lau1yach/service/impl/TestServiceImpl.java
@@ -1,4 +1,5 @@
 package com.lau1yach.service.impl;
+
 import com.lau1yach.mapper.TestMapper;
 import com.lau1yach.service.TestService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,16 +10,16 @@ import org.springframework.stereotype.Service;
  * @Title: TestServiceImpl
  * @ProjectName SSM_Store
  * @Description: TODO
- * @date 9/30/2018  5:42 PM
+ * @date 10/2/2018  10:41 PM
  */
 @Service
 public class TestServiceImpl implements TestService {
+
     @Autowired
     private TestMapper testMapper;
+
     @Override
     public String queryNow() {
-
         return testMapper.queryNow();
     }
-
 }
